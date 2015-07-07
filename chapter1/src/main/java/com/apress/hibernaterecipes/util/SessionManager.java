1aed5946f3c469f41ce476cfc54f27829df1737f
dlee0113 <dlee0113@gmail.com>
2015-07-07 11:29:08 -0400

    initial upload

---
+++ chapter1/src/main/java/com/apress/hibernaterecipes/util/SessionManager.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.apress.hibernaterecipes.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * @author sree
+ *
+ */
+public class SessionManager {
+	
+	private static final SessionFactory sessionFactory = buildSessionFactory(); 
+	 
+	private static SessionFactory buildSessionFactory() { 
+		try { 
+		 // Create the SessionFactory from hibernate.cfg.xml 
+			 Configuration configuration = new Configuration().configure();
+			 ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                     .applySettings(configuration.getProperties())
+                     .build();
+			 return configuration.buildSessionFactory(serviceRegistry);
+			 
+		} 
+		catch (Throwable ex) { 
+		 // Make sure you log the exception, as it might be swallowed 
+			 ex.printStackTrace(); 
+		 
+			 throw new ExceptionInInitializerError(ex); 
+		} 
+	} 
+	 
+	public static SessionFactory getSessionFactory() { 
+		return sessionFactory; 
+	} 
+
+
+}
